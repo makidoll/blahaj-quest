8fab3ceb87805ee2b1f1b8ccad331c26697a8eb7
Maki <mxmcube@gmail.com>
2022-04-30 00:30:26 -0500

    Added more countries

--- generate.js
+++ generate.js
@@ -47,21 +47,61 @@ async function getStock(countryCode, languageCode, itemCode) {
 }
 
 (async () => {
-	const blahajData = [
+	// TODO: use this https://en.wikipedia.org/wiki/List_of_countries_with_IKEA_stores
+
+	const blahajAmericasData = [
 		// north america
 		...(await getStock("us", "en", "90373590")),
 		...(await getStock("ca", "en", "90373590")),
 		// south america
 		...(await getStock("mx", "es", "90373590")),
-		// europe
-		...(await getStock("es", "es", "30373588")),
-		...(await getStock("gb", "en", "30373588")),
-		...(await getStock("fr", "fr", "30373588")),
-		...(await getStock("be", "nl", "30373588")),
-		...(await getStock("nl", "nl", "30373588")),
-		...(await getStock("de", "de", "30373588")),
-		...(await getStock("it", "it", "30373588")),
 	];
+
+	const blahajEuropeData = [
+		// https://www.worldometers.info/geography/how-many-countries-in-europe/
+		...(await getStock("ru", "ru", "30373588")), // russia
+		...(await getStock("de", "de", "30373588")), // germany
+		...(await getStock("gb", "en", "30373588")), // united kingdom
+		...(await getStock("fr", "fr", "30373588")), // france
+		...(await getStock("it", "it", "30373588")), // italy
+		...(await getStock("es", "es", "30373588")), // spain
+		...(await getStock("ua", "uk", "30373588")), // ukraine
+		...(await getStock("pl", "pl", "30373588")), // poland
+		...(await getStock("ro", "ro", "30373588")), // romania
+		...(await getStock("nl", "nl", "30373588")), // netherlands
+		...(await getStock("be", "nl", "30373588")), // belgium
+		...(await getStock("cz", "cs", "30373588")), // czech republic
+		// greece has a different api
+		...(await getStock("pt", "pt", "30373588")), // portugal
+		...(await getStock("se", "sv", "30373588")), // sweden
+		...(await getStock("hu", "hu", "30373588")), // hungary
+		// belarus has no ikea
+		...(await getStock("at", "de", "30373588")), // austria
+		...(await getStock("rs", "sr", "30373588")), // serbia
+		...(await getStock("ch", "en", "30373588")), // switzerland
+		// bulgaria has a different api
+		...(await getStock("dk", "da", "30373588")), // denmark
+		...(await getStock("fi", "fi", "30373588")), // finland
+		...(await getStock("sk", "sk", "30373588")), // slovakia
+		...(await getStock("no", "no", "30373588")), // norway
+		...(await getStock("ie", "en", "30373588")), // ireland
+		...(await getStock("hr", "hr", "30373588")), // croatia
+		// moldava has like fake ikea what
+		// lithuania has a different api
+		// north macedonia has no ikea
+		...(await getStock("si", "sl", "30373588")), // slovenia
+		// latvia has a different api
+		// montenegro has another fake ikea
+		// luxemberg has no ikea yet, until 2025
+		// iceland has a different api
+		// andora has no ikea
+		// monaco has no ikea
+		// liechtenstein has no ikea
+		// san marino has no ikea
+		// holy see has no ikea
+	];
+
+	const blahajData = [...blahajAmericasData, ...blahajEuropeData];
 
 	const publicPath = path.resolve(__dirname, "public");
 
